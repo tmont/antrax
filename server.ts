ff7fa1e608dd03ee6c062d596398d0593b9164ae
tmont <tommy.mont@gmail.com>
2025-12-20 12:13:59 -0800

    maybe fixed caching issues in dev

--- server.ts
+++ server.ts
@@ -1,22 +1,21 @@
 const server = Bun.serve({
+    development: true,
     port: 11000,
     routes: {
-        '/': new Response(Bun.file('./public/index.html')),
-        '/index.html': new Response(Bun.file('./public/index.html')),
-        '/app.js': new Response(Bun.file('./public/app.js')),
-        '/app.js.map': new Response(Bun.file('./public/app.js.map')),
-        '/app.css': new Response(Bun.file('./public/app.css')),
-        '/fonts/:font': req => {
-            const pathname = new URL(req.url).pathname.split('/')[2];
-            return new Response(Bun.file('./public/fonts/' + pathname));
-        },
-        '/images/*': req => {
+        '/': () => new Response(Bun.file('./public/index.html')),
+        '/index.html': () => new Response(Bun.file('./public/index.html')),
+        '/app.js': () => new Response(Bun.file('./public/app.js')),
+        '/app.js.map': () => new Response(Bun.file('./public/app.js.map')),
+        '/app.css': () => new Response(Bun.file('./public/app.css')),
+        '/:dir/*': req => {
+            const { dir } = req.params;
+            const allowed: Record<string, 1> = { assets: 1, images: 1, fonts: 1 };
+            if (!allowed[dir]) {
+                throw new Error('404');
+            }
+
             const pathname = new URL(req.url).pathname.split('/').slice(2).join('/');
-            return new Response(Bun.file('./public/images/' + pathname));
-        },
-        '/assets/*': req => {
-            const pathname = new URL(req.url).pathname.split('/').slice(2).join('/');
-            return new Response(Bun.file('./public/assets/' + pathname));
+            return new Response(Bun.file(`./public/${dir}/` + pathname));
         },
     },
 });
