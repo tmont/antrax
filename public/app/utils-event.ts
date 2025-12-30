fc98e5c7e221765d96daf841dc66f1d93f263fd7
tmont <tommy.mont@gmail.com>
2025-12-29 17:36:07 -0800

    fixed touches not being converted to coordinates
    fixes #120

--- public/app/utils-event.ts
+++ public/app/utils-event.ts
@@ -3,6 +3,12 @@ export interface ClientCoordinates {
     clientY: number;
 }
 
-export const touchToCoordinates = (e: TouchEvent): ClientCoordinates => e.touches.item(0) || { clientX: 0, clientY: 0 };
+export const touchToCoordinates = (e: TouchEvent): ClientCoordinates => {
+    const touch = e.touches.item(0);
+    return {
+        clientX: touch?.clientX || 0,
+        clientY: touch?.clientY || 0,
+    };
+};
 
 export const isLeftMouseButton = (e: MouseEvent): boolean => e.button === 0;
